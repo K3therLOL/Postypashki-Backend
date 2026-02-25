2e179ecda9dc4eb112a19cb7b35f0d0b3a7cadb3
K3therLOL <g3roylol@gmail.com>
2026-02-25 18:21:13 +0300

    implemented database manage of repo

--- 02-crypto-server/repository/database/database.go
+++ 02-crypto-server/repository/database/database.go
@@ -1,9 +1,12 @@
 package repository
 
 import (
+	"crypto/rand"
 	"cryptoserver/clean/domain"
 	"database/sql"
+	"encoding/binary"
 	"log"
+	"math"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
@@ -27,15 +30,29 @@ func NewStorage(connString string) *Database {
 	return &Database{db: db}
 }
 
+func generateRandomId() int32 {
+	var id uint64
+	binary.Read(rand.Reader, binary.BigEndian, &id)
+
+	return int32(id % uint64(math.MaxInt32))
+}
+
 func (s *Database) Save(user *domain.User) error {
-	return nil
+	query := `INSERT INTO users (user_id, username, password_hash) VALUES ($1, $2, $3);`
+	id := generateRandomId()
+	_, err := s.db.Exec(query, id, user.Username, user.PasswordHash)
+	log.Printf("user registered: %s --- %s\n", user.Username, user.PasswordHash)
+	return err
 }
 
 func (s *Database) Exist(username string) *domain.User {
-	passwordHash := ""
-	//hash, ok := r.s[username]
-	//if !ok {
-	//	return nil
-	//}
+	query := `SELECT username, password_hash FROM users WHERE username = $1;`
+
+	var passwordHash string
+	if err := s.db.QueryRow(query, username).Scan(&username, &passwordHash); err != nil {
+		return nil
+	}
+
+	log.Printf("user %s exists\n", username)
 	return &domain.User{Username: username, PasswordHash: passwordHash}
 }
