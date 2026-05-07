64b9db210bb239b325366d174e3713ee9c3fdb2a
K3therLOL <g3roylol@gmail.com>
2026-05-07 14:03:20 +0300

    added NewUser and NewSession functions

--- 03-task-server/clean/domain/auth/auth.go
+++ 03-task-server/clean/domain/auth/auth.go
@@ -2,18 +2,26 @@ package domain
 
 type User struct {
 	ID           int
-	Login        string
+	Username     string
 	PasswordHash string
+}
+
+func NewUser(username, passwordHash string) *User {
+	return &User{Username: username, PasswordHash: passwordHash}
 }
 
 type UserRepository interface {
 	Save(user *User) error
-	Exist(login string) *User
+	Exist(username string) *User
 }
 
 type Session struct {
 	userID    int
 	sessionID string
+}
+
+func NewSession() *Session {
+	return &Session{}
 }
 
 type SessionProvider interface {
