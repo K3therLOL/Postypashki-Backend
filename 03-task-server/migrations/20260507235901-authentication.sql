5bb1352f950fa954b73c279a27243d2643680180
K3therLOL <g3roylol@gmail.com>
2026-05-08 17:05:43 +0300

    changed migration tables

--- 03-task-server/migrations/20260507235901-authentication.sql
+++ 03-task-server/migrations/20260507235901-authentication.sql
@@ -10,12 +10,7 @@ CREATE TABLE sessions (
     session_id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
     token TEXT NOT NULL,
-    expires_at TIMESTAMP WITH TIME ZONE,
-
-    CONSTRAINT fk_user
-        FOREIGN KEY (user_id)
-        REFERENCES users(user_id)
-        ON DELETE CASCADE
+    expires_at TIMESTAMP WITH TIME ZONE
 );
 
 -- +migrate Down
