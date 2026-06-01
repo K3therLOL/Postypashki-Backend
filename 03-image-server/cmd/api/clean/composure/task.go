a3728e84d0d2045c099b9ded2a40ae17e2910fd4
K3therLOL <g3roylol@gmail.com>
2026-06-01 21:24:37 +0300

    changed redis layer to postgres

--- 03-image-server/cmd/api/clean/composure/task.go
+++ 03-image-server/cmd/api/clean/composure/task.go
@@ -1,14 +1,16 @@
 package composure
 
 import (
+	"os"
 	usecase "taskserver/clean/usecase/task"
-	resRepository "taskserver/repository/result/redis"
+	resRepository "taskserver/repository/result/postgres"
 	taskRepository "taskserver/repository/task/rai"
 )
 
 func NewTaskInteractor() *usecase.TaskInteractor {
 	taskRepo := taskRepository.NewTaskRepository()
-	resRepo := resRepository.NewResultRepository(24)
+	connString := os.Getenv("DB_CONN_STRING")
+	resRepo := resRepository.NewImageRepository(connString)
 	interactor := usecase.NewTaskInteractor(taskRepo, resRepo)
 	return interactor
 }
