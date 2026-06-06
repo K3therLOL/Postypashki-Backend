5b0236fe1076cf5017479d6dffce9b603400e126
K3therLOL <g3roylol@gmail.com>
2026-06-06 08:58:35 +0300

    fixed bugs in handling postgres requests from go

--- 03-image-server/cmd/api/repository/result/postgres/postgres.go
+++ 03-image-server/cmd/api/repository/result/postgres/postgres.go
@@ -38,33 +38,35 @@ func NewImageRepository(connString string) *ImageRepository {
 	return imageRepo
 }
 
-func (imageRepo *ImageRepository) delete(uuid string) error {
-	query := `DELETE FROM images WHERE uuid = $1;`
-	_, err := imageRepo.db.Exec(query, uuid)
+func (imageRepo *ImageRepository) delete(task_id string) error {
+	query := `DELETE FROM images WHERE task_id = $1;`
+	_, err := imageRepo.db.Exec(query, task_id)
 
 	if err == nil {
-		imageRepo.logger.Printf("Image with task uuid %s deleted from db.\n", uuid)
+		imageRepo.logger.Printf("Image with task task_id %s deleted from db.\n", task_id)
 	}
 
 	return err
 }
 
-func (imageRepo *ImageRepository) Get(uuid string) (string, error) {
-	query := `SELECT url, expires_at FROM images WHERE uuid = $1;`
+func (imageRepo *ImageRepository) Get(task_id string) (string, error) {
+	query := `SELECT url, expires_at FROM images WHERE task_id = $1;`
 
 	var (
 		imageUrl  string
 		expiresAt time.Time
 	)
 
-	if err := imageRepo.db.QueryRow(query, uuid).Scan(&imageUrl, &expiresAt); err != nil {
+	if err := imageRepo.db.QueryRow(query, task_id).Scan(&imageUrl, &expiresAt); err != nil {
+		imageRepo.logger.Println("Something happened with postgres.")
 		return "", err
 	}
 
-	if expiresAt.After(time.Now()) {
-		imageRepo.delete(uuid)
-		return "", ErrLinkExpired
-	}
+	//if expiresAt.After(time.Now()) {
+	//	imageRepo.logger.Printf("Link %s expired\n", imageUrl)
+	//	imageRepo.delete(task_id)
+	//	return "", ErrLinkExpired
+	//}
 
 	imageRepo.logger.Printf("Image (image_url -- %s, expires_at -- %v) fetched from db.\n",
 		imageUrl,
@@ -74,6 +76,6 @@ func (imageRepo *ImageRepository) Get(uuid string) (string, error) {
 	return imageUrl, nil
 }
 
-func (imageRepo *ImageRepository) Set(uuid, imageUrl string) error {
+func (imageRepo *ImageRepository) Set(task_id, imageUrl string) error {
 	return nil
 }
